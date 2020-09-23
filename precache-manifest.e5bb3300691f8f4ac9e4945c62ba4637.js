443c30a804874b789f474ab6decf4043a52fa7d7
IlyasIt <ilyas.musin.172001@mail.ru>
2020-09-23 16:05:06 +0300

    Updates

---
+++ precache-manifest.e5bb3300691f8f4ac9e4945c62ba4637.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dc5fb3c5aa3b7bb5f79ffbfb1679f6",
+    "url": "/near_city_game/index.html"
+  },
+  {
+    "revision": "d45794933324841f0130",
+    "url": "/near_city_game/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8d9cf553f6f88ae5ae03",
+    "url": "/near_city_game/static/css/main.69cc83b0.chunk.css"
+  },
+  {
+    "revision": "d45794933324841f0130",
+    "url": "/near_city_game/static/js/2.2af75274.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/near_city_game/static/js/2.2af75274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9cf553f6f88ae5ae03",
+    "url": "/near_city_game/static/js/main.7033bbea.chunk.js"
+  },
+  {
+    "revision": "3d8b167f4e317a3d722d",
+    "url": "/near_city_game/static/js/runtime-main.de5df6c3.js"
+  },
+  {
+    "revision": "bd78a5af8c0bb5adaef9147bb4b3d7f7",
+    "url": "/near_city_game/static/media/city.bd78a5af.jpg"
+  }
+]);
